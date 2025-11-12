75f0b8002ba0317c3a76440aa05bb0a3863ad7bf
OscarMadrazo <oscarmadrazo24@gmail.com>
2025-11-12 09:28:22 -0800

    Actualizacion de codigos para acceso y registro de usuarios

--- store/urls.py
+++ store/urls.py
@@ -6,8 +6,8 @@ from .views import contact, detail, register
 from .forms import LoginForm
 
 urlpatterns = [
-    path('contact/', contact, name = 'contact'),
+    path('contact/', contact, name='contact'),
     path('register/', register, name='register'),
-    path('login/', auth_views.LoginView.as_view(templates_name='store/login.html', authentication_form=LoginForm), name='login'),
-    path('detail/<int:pk>/', detail, name = 'detail'),
+    path('login/', auth_views.LoginView.as_view(template_name='store/login.html', authentication_form=LoginForm), name='login'),
+    path('detail/<int:pk>/', detail, name='detail'),
 ]
