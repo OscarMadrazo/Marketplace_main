e389cec0a71c1f82908d73cb106bf6424569ed4c
OscarMadrazo <oscarmadrazo24@gmail.com>
2025-11-20 07:45:02 -0800

    Cree el archivo form.html y actualice urls.py y forms.py

--- store/forms.py
+++ store/forms.py
@@ -58,35 +58,35 @@ class NewItemForm(forms.ModelForm):
         model = Item 
         fields = ['category', 'name', 'description', 'price', 'image']
 
-    widgets = {
-        'category': forms.Select(
-            attrs = {
-                'class': 'form-select'
-            }
-        ),
+        widgets = {
+            'category': forms.Select(
+                attrs = {
+                    'class': 'form-select'
+                }
+            ),
 
-        'name': forms.TextInput(
-            attrs = {
-                'class': 'form-select'
-            }
-        ),
+            'name': forms.TextInput(
+                attrs = {
+                    'class': 'form-select'
+                }
+            ),
 
-        'description': forms.Textarea(
-            attrs = {
-                'class': 'form-select',
-                'style': 'height: 100px'
-            }
-        ),
+            'description': forms.Textarea(
+                attrs = {
+                    'class': 'form-select',
+                    'style': 'height: 100px'
+                }
+            ),
 
-        'price': forms.TextInput(
-            attrs = {
-                'class': 'form-select'
-            }
-        ),
+            'price': forms.TextInput(
+                attrs = {
+                    'class': 'form-select'
+                }
+            ),
 
-        'image': forms.FileInput(
-            attrs = {
-                'class': 'form-select'
-            }
-        ),
-    }
+            'image': forms.FileInput(
+                attrs = {
+                    'class': 'form-select'
+                }
+            ),
+        }
